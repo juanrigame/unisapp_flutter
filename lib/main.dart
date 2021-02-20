1a87191927a6bb9ad956db1030d3410dfac10c28
Juan Ricardo <juan.ricardo.ga.me@gmail.com>
2021-02-20 17:19:39 -0600

    Agregado login
    login

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,81 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: loginDemo());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, home: loginDemo(context));
   }
 }
 
-Widget loginDemo() {
+Widget loginDemo(context) {
   return Scaffold(
+    backgroundColor: Colors.white,
     appBar: AppBar(
-      title: Text("Unisapp"),
+      title: Text("Acceso Unisapp"),
     ),
-    body: Container(),
+    body: SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 60.0),
+            child: Center(
+              child: Container(
+                  width: 200,
+                  height: 150,
+                  /*decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(50.0)),*/
+                  child: Image.asset('asset/images/schedule.png')),
+            ),
+          ),
+          Padding(
+            //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Matrícula',
+                  hintText: 'Introduce una matrícula dada de alta'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+                left: 15.0, right: 15.0, top: 15, bottom: 0),
+            //padding: EdgeInsets.symmetric(horizontal: 15),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Contraseña',
+                  hintText: 'Introducir contraseña'),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 50,
+            width: 250,
+            decoration: BoxDecoration(
+                color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+            child: FlatButton(
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (_) => HomePage()));
+              },
+              child: Text(
+                'Acceder',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
   );
+}
+
+Widget HomePage() {
+  return Container();
 }
 
 /*
